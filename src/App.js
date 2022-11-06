71bba91a178299b66cfb21676a953259a79e662b
Daniel Schiavoni <daniel.schiavoni@leanbakery.com>
2022-11-06 18:08:18 +0000

    create a player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Player from "./components/Player";
 
 function App() {
+  const [songs] = useState([
+    {
+      title: "Volver A los 17",
+      artist: "Milton Nascimento",
+      img_src: "./images/Geraes.jpg",
+      src: "./music/Volver A Los Diecisiete - Milton Nascimento  Mercedes Sosa.mp3"
+    },
+    {
+      title: "Take Five",
+      artist: "Dave Brubeck and The Dave Brubeck Quartet",
+      img_src: "./images/david brubeck quartet.jpg",
+      src: "./music/Dave Brubeck, The Dave Brubeck Quartet - Take Five.mp3"
+    }
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+
+  useEffect(()=>{
+    setNextSongIndex(()=> {
+      if (currentSongIndex + 1 > songs.length -1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
     </div>
   );
 }
